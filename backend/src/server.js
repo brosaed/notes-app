12ad5a605675305b46d960ef9b80dbd74e9a3159
brosaed <saedarab004@gmail.com>
2025-06-24 11:48:28 +0300

    created routers

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,10 @@
 import express from "express";
-
-// const express = require("express");
+import notesRoutes from "./routes/notesRoutes.js";
 
 const app = express();
 
-app.get("/api/notes", (req, res) => {
-    res.send("You got 10 Notes")
-});
+app.use("/api/notes", notesRoutes);
+
 
 app.listen(5002, () => {
   console.log("Server is running on PORT: 5002");
